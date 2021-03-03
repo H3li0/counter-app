96f76236250cb2777d557d3426191762ff2c4fea
Helio Junior <helios.junior@gmail.com>
2021-03-02 23:06:57 -0300

    Setting attributes of components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,8 +8,8 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Element</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Element</button>
       </React.Fragment>
     );
   }
