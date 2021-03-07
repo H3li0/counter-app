a777fb9d0c0cef9a10406cb7906e9552934bb78d
Helio Junior <helios.junior@gmail.com>
2021-03-07 16:37:19 -0300

    Single source of truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,8 +11,16 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    console.log("entrei em handleReset()");
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
-    console.log("handleDelete() event called", counterId);
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
   };
@@ -20,6 +28,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
