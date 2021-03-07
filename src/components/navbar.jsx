98ffb2358bcec1e925bcf937a92599863d10f889
Helio Junior <helios.junior@gmail.com>
2021-03-07 18:05:21 -0300

    Lifting the state Up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,16 @@ import React, { Component } from "react";
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
+      <React.Fragment>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {this.props.totalCounters}
+            </span>
+          </a>
+        </nav>
+      </React.Fragment>
     );
   }
 }
