c1bab9c8887be23452b3b26a9ca7bf751e6fac0a
Helio Junior <helios.junior@gmail.com>
2021-03-07 19:15:57 -0300

    Desctructuring arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 // Stateless Functional Component
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <React.Fragment>
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           Navbar{" "}
           <span className="badge badge-pill badge-secondary">
-            {props.totalCounters}
+            {totalCounters}
           </span>
         </a>
       </nav>
