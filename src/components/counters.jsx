6f66d28d7380daf7b7eeb668aa0d499b5d684757
Helio Junior <helios.junior@gmail.com>
2021-03-07 16:26:19 -0300

    Updating the state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,8 +11,10 @@ class Counters extends Component {
     ],
   };
 
-  handleDelete = () => {
-    console.log("handleDelete() event called");
+  handleDelete = (counterId) => {
+    console.log("handleDelete() event called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
   };
 
   render() {
@@ -22,8 +24,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
           />
         ))}
       </div>
